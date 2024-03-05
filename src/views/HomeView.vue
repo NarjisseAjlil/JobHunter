14db5d701d9cddec25471f948cf1603aa725d485
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-03-05 23:33:28 +0100

    changed homeview and moved it to website.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,15 +17,5 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="text-center">
-    <p class="h1 mb-5 text-success">JobHunter</p>
-  </div>
-    <div class="col-9 text-center all">
-      <h2 class=" mb-5">Stage optimisé, suivi simplifié.</h2>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-  </div>
   <Website></Website>
 </template>
